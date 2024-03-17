b1a8ab987df91f747ba62547e8ee95578a1d1e46
zarekchung <zarekchung@MacBook-Pro-3.local>
2024-03-17 14:51:40 +0800

    section 2

---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        var app = builder.Build();
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
